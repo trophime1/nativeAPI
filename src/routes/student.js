a324d433c009589a294ca7cf4d17b6c2eb534205
trophime1 <trophime250@gmail.com>
2022-12-14 18:50:32 +0200

    last update

--- src/routes/student.js
+++ src/routes/student.js
@@ -4,9 +4,9 @@ import studentController from "../controllers/student";
 const studentRouter = express.Router();
 
 studentRouter.get("/", studentController.getAll);
-studentRouter.post("student/create",studentController.createStudent);
-studentRouter.get("student/single/:id", studentController.getStudentById);
-studentRouter.patch("student/update/:id", studentController.updateStudent);
-studentRouter.delete("student/delete/:id",studentController.deleteStudent);
+studentRouter.post("/student/create",studentController.createStudent);
+studentRouter.get("/student/single/:id", studentController.getStudentById);
+studentRouter.patch("/student/update/:id", studentController.updateStudent);
+studentRouter.delete("/student/delete/:id",studentController.deleteStudent);
 
 export default studentRouter;
