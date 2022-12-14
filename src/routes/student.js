1738257d57995a7cc45c04361f7614ef1bee83e8
hicode-byte <hicode250@gmail.com>
2022-12-14 12:47:21 +0200

    final

--- src/routes/student.js
+++ src/routes/student.js
@@ -3,10 +3,10 @@ import studentController from "../controllers/student";
 
 const studentRouter = express.Router();
 
-studentRouter.get("/", studentController.getStudentById);
-studentRouter.post("/student/create",studentController.createStudent);
-studentRouter.get("/student/single/:id", studentController.getStudentById);
-studentRouter.patch("/student/update/:id", studentController.updateStudent);
-studentRouter.delete("/student/delete/:id",studentController.deleteStudent);
+studentRouter.get("/", studentController.getAll);
+studentRouter.post("student/create",studentController.createStudent);
+studentRouter.get("student/single/:id", studentController.getStudentById);
+studentRouter.patch("student/update/:id", studentController.updateStudent);
+studentRouter.delete("student/delete/:id",studentController.deleteStudent);
 
 export default studentRouter;
